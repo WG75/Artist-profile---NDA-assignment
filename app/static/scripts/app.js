548e43f6ffb0fc26cbb91e45e4e3a98b5c65dcc6
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-04-11 23:36:09 +0200

    styled the player

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('')
+const wavesurfer = WaveSurfer.create({
+  container: '.wave',
+  waveColor: 'white',
+  progressColor: 'green',
+  height: 70,
+  barHeight: 0.5,
+  barWidth: 1,
+  cursorWidth: 2,
+  cursorColor: '#464d50',
+  hideScrollbar: true
+
+})
+
+wavesurfer.load('https://s3-us-west-1.amazonaws.com/music-noirdoor/Fireworks.mp3')
+
+wavesurfer.on('ready', function () {
+    // wavesurfer.play();
+});
--- app/static/scripts/bundle.js
+++ app/static/scripts/bundle.js
@@ -73,7 +73,24 @@
 "use strict";
 
 
-console.log('');
+var wavesurfer = WaveSurfer.create({
+  container: '.wave',
+  waveColor: 'white',
+  progressColor: 'green',
+  height: 70,
+  barHeight: 0.5,
+  barWidth: 1,
+  cursorWidth: 2,
+  cursorColor: '#464d50',
+  hideScrollbar: true
+
+});
+
+wavesurfer.load('https://s3-us-west-1.amazonaws.com/music-noirdoor/Fireworks.mp3');
+
+wavesurfer.on('ready', function () {
+  // wavesurfer.play();
+});
 
 /***/ })
 /******/ ]);
